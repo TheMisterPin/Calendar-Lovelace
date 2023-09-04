249e3e0de5290b0f31a806ec64c434c7fdbf48a2
TheMisterPin <137160089+TheMisterPin@users.noreply.github.com>
2023-09-04 13:08:09 +0200

    event date time

--- src/utils/newEventHandler.ts
+++ src/utils/newEventHandler.ts
@@ -2,13 +2,15 @@
 
 const newEventDateInput: HTMLInputElement = document.querySelector('#newEventDate')!;
 const newEventTitleInput : HTMLInputElement= document.querySelector('#newEventTitle')!;
-const newEventTxtInput: HTMLInputElement = document.querySelector('#newEventText')!;
+const newEventTxtInput: HTMLInputElement = document.querySelector('#newEventText')!; 
+const newEventTimeInput: HTMLInputElement = document.querySelector('#newEventTime')!;
 
 
 export interface Event {
     title: string;
     date: string;
     txt: string;
+    time: string;
 }
 function saveEventToLocalStorage(event: Event): void {
     const localEvents = JSON.parse(localStorage.getItem('events') || '[]');
@@ -17,15 +19,17 @@ function saveEventToLocalStorage(event: Event): void {
 }
 
 
-export function newEventHandler(): { title: string, date: string, txt: string } {
+export function newEventHandler(): { title: string, date: string, txt: string, time: string} {
     const title = newEventTitleInput.value;
     const txt = newEventTxtInput.value;
     const date = newEventDateInput.value;
+    const time = newEventTimeInput.value;
     
    const newEvent: Event = {
         title,
         date,
         txt,
+        time,
     }
     saveEventToLocalStorage(newEvent);
 return newEvent
