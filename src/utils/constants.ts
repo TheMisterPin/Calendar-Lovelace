e33f636de8798eb2b41c0a39935d8e7aa8e72973
TheMisterPin <137160089+TheMisterPin@users.noreply.github.com>
2023-09-05 08:37:34 +0200

    deleted backgrounds

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,56 +1,52 @@
 interface MonthInfo {
   name: string;
-  background: string;
+
 }
 
 export const months: MonthInfo[] = [
   {
-    name: "January",
-    background: "url('../media/backgrounds/January.jpg')"
-  },
+    name: "January"
+     },
   {
     name: "February",
-    background: "url('../media/backgrounds/February.jpg')"
   },
   {
     name: "March",
-    background: "url('../media/backgrounds/March.jpg')"
   },
   {
     name: "April",
-    background: "url('../media/backgrounds/April.jpg')"
+ 
   },
   {
     name: "May",
-    background: "url('../media/backgrounds/May.jpg')"
+ 
   },
   {
     name: "June",
-    background: "url('../media/backgrounds/June.jpg')"
+   
   },
   {
     name: "July",
-    background: "url('../media/backgrounds/July.jpg')"
-  },
+  },  
   {
     name: "August",
-    background: "url('../media/backgrounds/August.jpg')"
+ 
   },
   {
     name: "September",
-    background: "url('../media/backgrounds/September.jpg')"
+  
   },
   {
     name: "October",
-    background: "url('../media/backgrounds/October.jpg')"
+  
   },
   {
     name: "November",
-    background: "url('../media/backgrounds/November.jpg')"
+  
   },
   {
     name: "December",
-    background: "url('../media/backgrounds/December.jpg')"
+  
   }
 ];
 
