27a7520596bd5bcb277d943c9e4a78e32c21b03d
TheMisterPin <137160089+TheMisterPin@users.noreply.github.com>
2023-09-08 09:50:30 +0200

    initial commit

--- dist/components/calendar.js
+++ dist/components/calendar.js
@@ -86,7 +86,7 @@ function processHolidays(holidays) {
         }
     }
 }
-export function populateCalendar(currentDate) {
+export function populateCalendar(currentDate, label) {
     clearCalendar();
     updateMonthHeader(currentDate);
     populateDays(currentDate);
---
+++ dist/utils/filter.js
@@ -0,0 +1,22 @@
+import { populateCalendar } from "../components/calendar";
+document.getElementById('labelFilter').addEventListener('change', (e) => {
+    const selectedLabel = e.target.value;
+    const label = selectedLabel;
+    populateCalendar(currentDate, label);
+});
+export function populateLabelFilter() {
+    const labels = JSON.parse(localStorage.getItem('eventLabels') || '[]');
+    const labelFilterDropdown = document.querySelector('#labelFilter');
+    labels.forEach((labelObj) => {
+        const option = document.createElement('option');
+        option.value = labelObj.name;
+        option.innerText = labelObj.name;
+        option.style.color = labelObj.color;
+        if (labelFilterDropdown) {
+            labelFilterDropdown.appendChild(option);
+        }
+    });
+}
+let currentDate = new Date;
+export function applyLabelFilter() {
+}
